bf4e0cff681bc92a38af910edc12e1736d81e2ff
Peter Ihimire <peterihimire@gmail.com>
2022-02-05 21:00:56 +0100

    Still working on the filter-by-value search function

--- store/reducers/templates.js
+++ store/reducers/templates.js
@@ -9,6 +9,7 @@ const localStorageTemplate = () => {
 const initialState = {
   // allTemplates: JSON.parse(localStorage.getItem("templates")) || [],
   allTemplates: localStorageTemplate,
+  appliedTemplates: [],
   filteredTemplates: [],
   loading: false,
   error: null,
@@ -34,16 +35,37 @@ const reducer = (state = initialState, action) => {
       };
 
     case actionTypes.FILTER_BY_VALUE:
+      // let newState = Object.assign({}, state);
+
+      let newState = { ...state };
       let value = action.payload.value;
       console.log(value);
       let filteredValues = state.allTemplates.filter((templates) => {
         console.log(templates.name);
-        return  templates.name.toLowerCase().includes(value);
+        return templates.name.toLowerCase().includes(value);
       });
+      // console.log(filteredValues);
+      let appliedTemplates = state.appliedTemplates;
+
+      if (value) {
+        appliedTemplates = addFilterIfNotExists(
+          actionTypes.FILTER_BY_VALUE,
+          appliedTemplates,
+        );
+        newState.filteredTemplates = filteredValues;
+      } else {
+        appliedTemplates = removeFilter(
+          actionTypes.FILTER_BY_VALUE,
+          appliedTemplates,
+        );
+        if (appliedTemplates.length === 0) {
+          newState.filteredTemplates = newState.allTemplates;
+        }
+      }
 
       return {
-        ...state,
-        filteredTemplates: filteredValues,
+        newState,
+        // allTemplates: filteredValues,
       };
 
     default:
@@ -52,3 +74,22 @@ const reducer = (state = initialState, action) => {
 };
 
 export default reducer;
+
+function addFilterIfNotExists(filter, appliedFilters) {
+  console.log(filter);
+  console.log(appliedFilters);
+  let index = appliedFilters.indexOf(filter);
+  console.log(index);
+  if (index === -1) appliedFilters.push(filter);
+
+  return appliedFilters;
+}
+
+function removeFilter(filter, appliedFilters) {
+  console.log(filter);
+  console.log(appliedFilters);
+  let index = appliedFilters.indexOf(filter);
+  console.log(index);
+  appliedFilters.splice(index, 1);
+  return appliedFilters;
+}
